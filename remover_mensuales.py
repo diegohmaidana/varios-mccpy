cc95fc09dc0c90778f303a49e35349d22fb7f312
gustavo <gsolmoirago@gmail.com>
2016-02-05 09:43:38 -0300

    se agrego remover_mensuales.py

---
+++ remover_mensuales.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
+import os
+import locale
+import shutil
+from datetime import date, timedelta
+
+path_inicio = '/bases/'
+meses = {'ene', 'feb', 'mar', 'abr',
+         'may', 'jun', 'jul', 'ago',
+         'sep', 'oct', 'nov', 'dic', 'dir'
+         }
+
+
+def borrar_meses_ant():
+    ''' Busca y borra las carpetas mensuales, menos del mes anterior
+    '''
+    locale.setlocale(locale.LC_ALL, "")
+    # Obtine el mes anterior como string
+    mes_anterior = (date.today() - timedelta(days=28)).strftime('%b')
+    for base, dirs, files in os.walk(path_inicio):
+        if dirs != []:
+            for dir in dirs:
+                if (dir in meses) and (dir != mes_anterior):
+                    shutil.rmtree(base + '/' + dir)
+
+
+if __name__ == '__main__':
+    borrar_meses_ant()
